4b10eb22b335d743a0a4d047067987ac048ad5b0
arif12904 <149857196+arif12904@users.noreply.github.com>
2025-04-08 19:25:39 +0800

    Add files via upload

---
+++ Posttest 3/src/data/Orang.java
@@ -0,0 +1,17 @@
+package data;
+
+public class Orang {
+    protected String nama;
+
+    public Orang(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
